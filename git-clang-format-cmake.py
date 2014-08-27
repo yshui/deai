47dbbb1416a82c0ce97d4c996246532331757bd7
Kenneth Benzie <k.benzie83@gmail.com>
2014-08-27 22:39:58 +0100

    Add CMakeLists.txt and git-pre-commit-hook, rework git-clang-format-cmake.py
    Adding CMakeLists.txt with add_subdirectory enables two features, the
    first is installing a git pre-commit hook (a thin wrapper around
    git-clang-format-cmake.py with correct invocation) to block any commits
    where source files have not been formatted, and a custom cmake target
    called 'format' which is responsible for formatting all edited files
    (again calling git-clang-format-cmake.py with the alternative invocation).
    
    At cmake time the paths to git, python, and clang-format are searched,
    failure to find these executables on your path will results in a cmake
    error. Alternatively it is possible to set one or all of GCFC_GIT_PATH,
    GCFC_PYTHON_PATH, and GCFC_CLANGFORMAT_PATH manually, a warning will be
    produced if the file path does not exist. The decision not to cause a
    cmake error is to enable the path to any or all of these dependencies
    to be built in your cmake tree, however this is currently untested and
    is considered a future feature at this time.
    
    Minor changes were required to the git-clang-format-cmake.py script to
    enable correct behaviour when invoked from the pre-commit hook and the
    cmake format target.

---
+++ git-clang-format-cmake.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+
+
+import os
+import subprocess
+import sys
+
+
+Git='git'
+ClangFormat='clang-format'
+
+
+def getGitHead():
+    RevParse = subprocess.Popen([Git, 'rev-parse', '--verify', 'HEAD'],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    RevParse.communicate()
+    if RevParse.returncode:
+        return '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+    else:
+        return 'HEAD'
+
+
+def getEditedFiles():
+    Head = getGitHead()
+    DiffIndex = subprocess.Popen(
+        [Git, 'diff-index', '--diff-filter=ACMR', '--name-only', Head],
+        stdout=subprocess.PIPE)
+    DiffIndexRet = DiffIndex.stdout.read()
+    return DiffIndexRet.split('\n')
+
+
+def isFormattable(File):
+    Extension = os.path.splitext(File)[1]
+    for Ext in ['.h', '.cpp', '.hpp', '.c', '.cc', '.hh', '.cxx', '.hxx']:
+        if Ext == Extension:
+            return True
+    return False
+
+
+def formatFile(FileName, InPlace):
+    if InPlace:
+        print("Formatting: " + FileName)
+        subprocess.Popen([ClangFormat, '-style=file', '-i', FileName])
+        return ""
+    else:
+        ClangFormatRet = subprocess.Popen(
+            [ClangFormat, '-style=file', FileName], stdout=subprocess.PIPE)
+        return ClangFormatRet.stdout.read()
+
+
+def requiresFormat(FileName, FormatContent):
+    File = open(FileName)
+    Content = File.read()
+    File.close()
+    if FormatContent == Content:
+        return False
+    return True
+
+
+def printUsageAndExit():
+    print("Usage: " + sys.argv[0] + " [--pre-commit|--cmake] " +
+          "[<path/to/git>] [<path/to/clang-format]")
+    sys.exit(1)
+
+
+def parseExecutablePathOption(Option):
+    if "git" in Option:
+        Git = Option
+    elif "clang-format" in Option:
+        ClangFormat = Option
+    else:
+        printUsageAndExit()
+
+
+if __name__ == "__main__":
+    if 2 > len(sys.argv):
+        printUsageAndExit()
+
+    if "--pre-commit" == sys.argv[1]:
+        InPlace = False
+    elif "--cmake" == sys.argv[1]:
+        InPlace = True
+    else:
+        printUsageAndExit()
+
+    if 3 == len(sys.argv):
+        parseExecutablePathOption(sys.argv[2])
+
+    if 4 == len(sys.argv):
+        parseExecutablePathOption(sys.argv[3])
+
+    EditedFiles = getEditedFiles()
+    FormatResults = {}
+    for FileName in EditedFiles:
+        if isFormattable(FileName):
+            FormatResults[FileName] = formatFile(FileName, InPlace)
+
+    ReturnCode = 0
+    if not InPlace:
+        for Result in FormatResults.items():
+            FileName = Result[0]
+            if requiresFormat(FileName, Result[1]):
+                print("'" + FileName +
+                      "' must be formatted, run the cmake target 'format'")
+                ReturnCode = 1
+
+    sys.exit(ReturnCode)
