655e1a7c9d8552e5b24bc972ddd55a6f2483a32d
Jens Breitbart <jbreitbart@gmail.com>
2015-02-05 15:51:20 +0100

    Fixes a problem in case GCF_IGNORE_LIST is not set.

--- git-cmake-format.py
+++ git-cmake-format.py
@@ -39,7 +39,7 @@ def getEditedFiles(InPlace):
 
 def isFormattable(File):
     for Dir in IgnoreList:
-        if '' != os.path.commonprefix([os.path.relpath(File), os.path.relpath(Dir)]):
+        if '' != Dir and '' != os.path.commonprefix([os.path.relpath(File), os.path.relpath(Dir)]):
             return False
     Extension = os.path.splitext(File)[1]
     for Ext in ['.h', '.cpp', '.hpp', '.c', '.cc', '.hh', '.cxx', '.hxx']:
