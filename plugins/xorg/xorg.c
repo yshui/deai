ac6177d85b7402e817b9ba48e3ff86cf43fdaa2e
Yuxuan Shui <yshuiv7@gmail.com>
2017-05-06 16:12:03 -0400

    xorg: add support for setting RESOURCE_MANAGER property
    aka xrdb suport

--- plugins/xorg/xorg.c
+++ plugins/xorg/xorg.c
@@ -9,8 +9,9 @@
 #include <helper.h>
 #include <log.h>
 
-#include <xcb/xcb.h>
 #include <assert.h>
+#include <stdio.h>
+#include <xcb/xcb.h>
 
 #include "uthash.h"
 #include "utils.h"
@@ -25,10 +26,12 @@ struct di_atom_entry {
 	UT_hash_handle hh, hh2;
 };
 
+define_trivial_cleanup_t(xcb_generic_error_t);
+
 static void di_xorg_ioev(struct di_listener_data *l) {
 	struct di_xorg_connection *dc = (void *)l->user_data;
-	//di_get_log(dc->x->di);
-	//di_log_va((void *)log, DI_LOG_DEBUG, "xcb ioev\n");
+	// di_get_log(dc->x->di);
+	// di_log_va((void *)log, DI_LOG_DEBUG, "xcb ioev\n");
 
 	xcb_generic_event_t *ev;
 
@@ -55,7 +58,8 @@ const char *di_xorg_get_atom_name(struct di_xorg_connection *xc, xcb_atom_t atom
 		return NULL;
 
 	ae = tmalloc(struct di_atom_entry, 1);
-	ae->name = strndup(xcb_get_atom_name_name(r), xcb_get_atom_name_name_length(r));
+	ae->name =
+	    strndup(xcb_get_atom_name_name(r), xcb_get_atom_name_name_length(r));
 	ae->atom = atom;
 	free(r);
 
@@ -65,7 +69,8 @@ const char *di_xorg_get_atom_name(struct di_xorg_connection *xc, xcb_atom_t atom
 	return ae->name;
 }
 
-xcb_atom_t di_xorg_intern_atom(struct di_xorg_connection *xc, const char *name, xcb_generic_error_t **e) {
+xcb_atom_t di_xorg_intern_atom(struct di_xorg_connection *xc, const char *name,
+                               xcb_generic_error_t **e) {
 	struct di_atom_entry *ae = NULL;
 	*e = NULL;
 
@@ -73,7 +78,8 @@ xcb_atom_t di_xorg_intern_atom(struct di_xorg_connection *xc, const char *name, 
 	if (ae)
 		return ae->atom;
 
-	auto r = xcb_intern_atom_reply(xc->c, xcb_intern_atom(xc->c, 0, strlen(name), name), e);
+	auto r = xcb_intern_atom_reply(
+	    xc->c, xcb_intern_atom(xc->c, 0, strlen(name), name), e);
 	if (!r)
 		return 0;
 
@@ -110,6 +116,40 @@ static void di_xorg_free_connection(struct di_xorg_connection *xc) {
 	}
 }
 
+static char *di_xorg_get_resource(struct di_xorg_connection *xc) {
+	auto scrn = screen_of_display(xc->c, xc->dflt_scrn);
+	auto r = xcb_get_property_reply(
+	    xc->c, xcb_get_property(xc->c, 0, scrn->root, XCB_ATOM_RESOURCE_MANAGER,
+	                            XCB_ATOM_ANY, 0, 0),
+	    NULL);
+	if (!r)
+		return strdup("");
+
+	auto real_size = r->bytes_after;
+	free(r);
+
+	r = xcb_get_property_reply(
+	    xc->c, xcb_get_property(xc->c, 0, scrn->root, XCB_ATOM_RESOURCE_MANAGER,
+	                            XCB_ATOM_ANY, 0, real_size),
+	    NULL);
+	if (!r)
+		return strdup("");
+
+	char *ret =
+	    strndup(xcb_get_property_value(r), xcb_get_property_value_length(r));
+	free(r);
+	return ret;
+}
+
+static void di_xorg_set_resource(struct di_xorg_connection *xc, const char *rdb) {
+	auto scrn = screen_of_display(xc->c, xc->dflt_scrn);
+	with_cleanup_t(xcb_generic_error_t) e = xcb_request_check(
+	    xc->c, xcb_change_property(xc->c, XCB_PROP_MODE_REPLACE, scrn->root,
+	                               XCB_ATOM_RESOURCE_MANAGER, XCB_ATOM_STRING, 8,
+	                               strlen(rdb), rdb));
+	(void)e;
+}
+
 static struct di_object *di_xorg_connect(struct di_xorg *x) {
 	struct di_xorg_connection *dc =
 	    di_new_object_with_type(struct di_xorg_connection);
@@ -129,6 +169,14 @@ static struct di_object *di_xorg_connect(struct di_xorg *x) {
 	di_register_typed_method(
 	    (void *)dc, di_create_typed_method((di_fn_t)di_xorg_get_xinput,
 	                                       "__get_xinput", DI_TYPE_OBJECT, 0));
+
+	di_register_typed_method(
+	    (void *)dc, di_create_typed_method((di_fn_t)di_xorg_get_resource,
+	                                       "__get_xrdb", DI_TYPE_STRING, 0));
+
+	di_register_typed_method(
+	    (void *)dc, di_create_typed_method((di_fn_t)di_xorg_set_resource,
+	                                       "__set_xrdb", DI_TYPE_VOID, 1, DI_TYPE_STRING));
 
 	di_register_typed_method(
 	    (void *)dc, di_create_typed_method((di_fn_t)di_xorg_free_connection,
