eb962ffcb7804f89d8dfc9e41c066c76dd4d1015
Yuxuan Shui <yshuiv7@gmail.com>
2017-05-08 14:45:49 -0400

    event: change the interface of the timer a bit

--- event.c
+++ event.c
@@ -4,11 +4,11 @@
 
 /* Copyright (c) 2017, Yuxuan Shui <yshuiv7@gmail.com> */
 
-#include "event_internal.h"
 #include "di_internal.h"
+#include "event_internal.h"
 #include "utils.h"
-#include <ev.h>
 #include <deai.h>
+#include <ev.h>
 struct di_ioev {
 	struct di_object;
 	ev_io evh;
@@ -36,6 +36,7 @@ static void di_ioev_callback(EV_P_ ev_io *w, int revents) {
 
 static void di_timer_callback(EV_P_ ev_timer *t, int revents) {
 	auto d = container_of(t, struct di_timer, evt);
+	ev_timer_stop(d->loop, t);
 	di_emit_signal_v((void *)d, "elapsed", ev_now(EV_A));
 }
 
@@ -52,6 +53,13 @@ static void di_ioev_dtor(struct di_object *obj) {
 static void di_timer_dtor(struct di_object *obj) {
 	struct di_timer *ev = (void *)obj;
 	ev_timer_stop(ev->loop, &ev->evt);
+}
+static void di_timer_again(struct di_timer *obj) {
+	ev_timer_again(obj->loop, &obj->evt);
+}
+static void di_timer_set(struct di_timer *obj, uint64_t t) {
+	obj->evt.repeat = t;
+	ev_timer_again(obj->loop, &obj->evt);
 }
 static struct di_object *di_create_ioev(struct di_object *obj, int fd, int t) {
 	struct di_evmodule *em = (void *)obj;
@@ -85,24 +93,36 @@ static struct di_object *di_create_timer(struct di_object *obj, uint64_t timeout
 	auto ret = di_new_object_with_type(struct di_timer);
 	ret->loop = em->loop;
 
-	auto dtor =
-	    di_create_typed_method((di_fn_t)di_timer_dtor, "__dtor", DI_TYPE_VOID, 0);
-	di_register_typed_method((void *)ret, dtor);
+	di_register_typed_method((void *)ret,
+	                         di_create_typed_method((di_fn_t)di_timer_dtor,
+	                                                "__dtor", DI_TYPE_VOID, 0));
+	di_register_typed_method((void *)ret,
+	                         di_create_typed_method((di_fn_t)di_timer_again,
+	                                                "start", DI_TYPE_VOID, 0));
+	di_register_typed_method((void *)ret,
+	                         di_create_typed_method((di_fn_t)di_timer_again,
+	                                                "again", DI_TYPE_VOID, 0));
 
-	ev_timer_init(&ret->evt, di_timer_callback, timeout, 0);
+	// Set the timeout and restart the timer
+	di_register_typed_method((void *)ret,
+	                         di_create_typed_method((di_fn_t)di_timer_set,
+	                                                "set", DI_TYPE_VOID, 1, DI_TYPE_UINT));
+
+	ev_init(&ret->evt, di_timer_callback);
+	ret->evt.repeat = timeout;
 	di_register_signal((void *)ret, "elapsed", 1, DI_TYPE_FLOAT);
-	ev_timer_start(em->loop, &ret->evt);
 	return (void *)ret;
 }
 void di_init_event_module(struct deai *di) {
 	auto em = di_new_module_with_type("event", struct di_evmodule);
 
-	auto fn = di_create_typed_method((di_fn_t)di_create_ioev, "fdevent",
-	                       DI_TYPE_OBJECT, 2, DI_TYPE_NINT, DI_TYPE_NINT);
+	auto fn =
+	    di_create_typed_method((di_fn_t)di_create_ioev, "fdevent",
+	                           DI_TYPE_OBJECT, 2, DI_TYPE_NINT, DI_TYPE_NINT);
 	di_register_typed_method((void *)em, (void *)fn);
 
 	fn = di_create_typed_method((di_fn_t)di_create_timer, "timer",
-	                       DI_TYPE_OBJECT, 1, DI_TYPE_UINT);
+	                            DI_TYPE_OBJECT, 1, DI_TYPE_UINT);
 	di_register_typed_method((void *)em, (void *)fn);
 	em->loop = di->loop;
 	di_register_module(di, (void *)em);
