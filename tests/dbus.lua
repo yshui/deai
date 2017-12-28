4d67cab6a01c41b533544d5ce1f3c764905b07f4
Yuxuan Shui <yshuiv7@gmail.com>
2017-12-28 13:24:32 -0500

    Test dbus

---
+++ tests/dbus.lua
@@ -0,0 +1,13 @@
+di.load_plugin("./plugins/dbus/di_dbus.so")
+local dbusl = di.spawn.run({"dbus-daemon", "--print-address", "--session", "--fork"}, false)
+dbusl.on("stdout_line", function(l)
+    di.env.DBUS_SESSION_BUS_ADDRESS = l
+    di.env.DBUS_SESSION_BUS_PID = nil
+    di.env.DBUS_SESSION_BUS_WINDOWID = nil
+end)
+dbusl.on("exit", function()
+    di.dbus.session_bus.get("org.freedesktop.DBus", "/org/freedesktop/DBus").Introspect().on("reply", function(s)
+        print(s)
+        collectgarbage()
+    end)
+end)
