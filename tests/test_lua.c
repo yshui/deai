956e5baac3f8b290806384b7c3944b778d7db4d7
Yuxuan Shui <yshuiv7@gmail.com>
2020-10-05 21:23:50 +0100

    tests: make sure we are really testings the plugins we just built
    In case the system plugins are loaded, test_lua will remove the module
    members from deai first.
    
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- tests/test_lua.c
+++ tests/test_lua.c
@@ -3,7 +3,14 @@
 #include <deai/helper.h>
 
 PUBLIC int di_plugin_init(struct deai *di) {
+	di_remove_member((struct di_object *)di, "lua");
+	di_remove_member((struct di_object *)di, "xorg");
+	di_remove_member((struct di_object *)di, "file");
+	di_remove_member((struct di_object *)di, "dbus");
 	di_call(di, "load_plugin", (char *)"./plugins/lua/di_lua.so");
+	di_call(di, "load_plugin", (char *)"./plugins/xorg/di_xorg.so");
+	di_call(di, "load_plugin", (char *)"./plugins/file/di_file.so");
+	di_call(di, "load_plugin", (char *)"./plugins/dbus/di_dbus.so");
 	di_getmi(di, lua);
 	assert(luam);
 
