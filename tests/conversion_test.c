5e28921445108b4b6c3352a33a0823418bfcbb56
Yuxuan Shui <yshuiv7@gmail.com>
2020-10-05 23:10:18 +0100

    tests: test if const char * -> char * conversion is handled
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- tests/conversion_test.c
+++ tests/conversion_test.c
@@ -7,12 +7,21 @@
 static void takes_string(void *obj unused, char *str unused) {
 }
 
+static void takes_string_and_modify(void *obj unused, char *str) {
+	// Make sure there is space for write
+	DI_CHECK(strlen(str) >= 1);
+	*str = '\0';
+}
+
 PUBLIC int di_plugin_init(struct deai *di) {
 	DI_CHECK_OK(di_method(di, "takes_string", takes_string, char *));
+	DI_CHECK_OK(di_method(di, "takes_string_and_modify", takes_string_and_modify, char *));
 
 	di_type_t retty;
 	void *retval;
 	di_callx((struct di_object *)di, "takes_string", &retty, &retval,
 	         DI_TYPE_STRING_LITERAL, "a string", DI_LAST_TYPE);
+
+	di_call(di, "takes_string_and_modify", (const char *)"string literal");
 	return 0;
 }
