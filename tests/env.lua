6c463567eb4a02d6c68fdbe6b4863199e83b449b
Yuxuan Shui <yshuiv7@gmail.com>
2020-10-09 21:25:08 +0100

    tests: fix env.lua
    Event listenr signature changed.
    
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- tests/env.lua
+++ tests/env.lua
@@ -3,8 +3,9 @@ di.os.env.PATH = "/non-existent"
 print(di.os.env.PATH)
 
 e = di.spawn.run({"ls"}, true)
-e.on("exit", function(e, ec, sig)
+e.on("exit", function(ec, sig)
     print(ec, sig)
+    assert(ec == 1)
 end)
 
 di.os.env.PATH = nil
