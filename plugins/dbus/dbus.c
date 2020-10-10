2c8c98d5d103e3e41b1dabfaf292b0a056aa09ae
Yuxuan Shui <yshuiv7@gmail.com>
2020-10-10 01:33:48 +0100

    plugins: dbus: fix use-after-free
    di_dbus_get_object is called with a unowned string literal, which is
    later captured by a closure in di_bus_watch_name.
    
    If the string is freed before the closure is called, there will be a
    use-after-free.
    
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- plugins/dbus/dbus.c
+++ plugins/dbus/dbus.c
@@ -142,7 +142,9 @@ static void di_dbus_watch_name(_di_dbus_connection *c, const char *busname) {
 	auto ret = di_dbus_send(c, msg);
 	dbus_message_unref(msg);
 
-	auto cl = di_closure(di_dbus_update_name_from_msg, false, ((void *)c, busname), void *);
+	char *busname_owned = strdup(busname);
+	auto cl = di_closure(di_dbus_update_name_from_msg, false,
+	                     ((void *)c, busname_owned), void *);
 	di_listen_to_once(ret, "reply", (struct di_object *)cl, true);
 	di_unref_object((void *)cl);
 	di_unref_object((void *)ret);
