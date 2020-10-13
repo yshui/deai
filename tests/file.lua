0e27762c047cc62058b0f664ce478adde94fa2db
Yuxuan Shui <yshuiv7@gmail.com>
2020-10-13 01:28:00 +0100

    tests: file: drop listen handles when done
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- tests/file.lua
+++ tests/file.lua
@@ -1,8 +1,9 @@
 di:load_plugin("./plugins/file/di_file.so")
 
 md = di.spawn:run({"mkdir", "testdir"}, true)
+listen_handles = {}
 
-md:on("exit", function()
+table.insert(listen_handles, md:on("exit", function()
 w = di.file:watch({"testdir"})
 
 function sigh(ev)
@@ -16,7 +17,7 @@ events = {"create", "access", "attrib", "close-write", "close-nowrite",
 "moved-to", "moved-from"}
 
 for _, i in pairs(events) do
-    w:on(i, sigh(i))
+    table.insert(listen_handles, w:on(i, sigh(i)))
 end
 
 fname = "./testdir/testfile"
@@ -38,14 +39,19 @@ function run_one(i)
         print("running ", unpack(cmds[i]))
         c = di.spawn:run(cmds[i], true)
         if i < #cmds then
-            c:on("exit", run_one(i+1))
+            table.insert(listen_handles, c:on("exit", run_one(i+1)))
         else
             w:remove("testdir")
             w:stop()
             w = nil
+            for _, lh in pairs(listen_handles) do
+                -- Stop all listeners, so the callback functions can be freed,
+                -- which in turn frees the lua script object
+                lh:stop()
+            end
             collectgarbage()
         end
     end
 end
 run_one(1)()
-end)
+end))
