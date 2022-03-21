ddbd7088587167cf62d9d5b9579764f3d20d6b48
Yuxuan Shui <yshuiv7@gmail.com>
2022-03-21 02:17:00 +0000

    tests: fix file.lua hang
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- tests/file.lua
+++ tests/file.lua
@@ -6,9 +6,16 @@ md = di.spawn:run({"mkdir", "testdir"}, true)
 md:once("exit", function()
 w = di.file:watch({"testdir"})
 
+event_count = 0
 function sigh(ev)
     return function(name, path)
         print("event: "..ev, name, path)
+        event_count = event_count + 1
+        if event_count == 14 then
+            print("all events received")
+            listen_handles = nil
+            collectgarbage()
+        end
     end
 end
 
@@ -18,7 +25,7 @@ events = {"create", "access", "attrib", "close-write", "close-nowrite",
 
 listen_handles = {}
 for _, i in pairs(events) do
-    handle = w:on(i, sigh(i))
+    local handle = w:on(i, sigh(i))
     handle:auto_stop(1)
     table.insert(listen_handles, handle)
 end
@@ -40,14 +47,9 @@ cmds = {
 function run_one(i)
     return function()
         print("running ", unpack(cmds[i]))
-        c = di.spawn:run(cmds[i], true)
+        local c = di.spawn:run(cmds[i], true)
         if i < #cmds then
             c:once("exit", run_one(i+1))
-        else
-            w:remove("testdir")
-            w = nil
-            listen_handles = nil
-            collectgarbage()
         end
     end
 end
