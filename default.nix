6e5f8c131110a0a33aac0c01ea0321a769c4394d
Yuxuan Shui <yshuiv7@gmail.com>
2024-07-05 18:05:31 +0100

    nix: build docs
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- default.nix
+++ default.nix
@@ -3,20 +3,53 @@
 , ninja
 , pkgconf
 , dbus
+, rustPlatform
 , libffi
 , libxcb
 , libXau
 , libXdmcp
 , libev
 , libxkbcommon
+, llvmPackages_17
 , lua
 , systemdLibs
+, cargo
+, sphinx
+, rustc
+, python3
 , xcbutilkeysyms }:
-
+let
+  python = python3.withPackages (p: [
+    p.sphinx
+    p.sphinx_rtd_theme
+  ]);
+  cargoVendor = rustPlatform.importCargoLock {
+    lockFile = ./nix/scanner.lock;
+  };
+in
 stdenv.mkDerivation {
   src = ./.;
   name = "deai";
-  nativeBuildInputs = [ meson ninja pkgconf ];
+  nativeBuildInputs = [ meson ninja pkgconf cargo rustc python llvmPackages_17.clang ];
   buildInputs = [ libev libffi libxcb libXau libXdmcp xcbutilkeysyms libxkbcommon dbus systemdLibs lua ];
+  env = {
+    LLVM_CONFIG_PATH = "${llvmPackages_17.libllvm.dev}/bin/llvm-config";
+    LIBCLANG_PATH = "${llvmPackages_17.libclang.lib}/lib";
+  };
+  outputs = [ "out" "doc" ];
   passthru.providedSessions = [ "deai" ];
+  prePatch = ''
+    mkdir docs/scanner/.cargo
+    echo "[source.crates-io]
+replace-with = \"vendored-sources\"
+[source.vendored-sources]
+directory = \"${cargoVendor}\"
+[net]
+offline = true" > docs/scanner/.cargo/config.toml
+    cp nix/scanner.lock docs/scanner/Cargo.lock
+  '';
+  postBuild = ''
+    mkdir -p $doc/share/doc/deai
+    python -m sphinx ../docs $doc/share/doc/deai/html
+  '';
 }
--- flake.nix
+++ flake.nix
@@ -3,8 +3,13 @@
   let 
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    deai = pkgs.callPackage ./default.nix {};
+
   in rec {
-    packages.${system}.deai = pkgs.callPackage ./default.nix {};
+    packages.${system}.deai = deai;
+    devShell.${system} = deai.overrideAttrs (o: {
+      nativeBuildInputs = o.nativeBuildInputs ++ [ pkgs.clippy ];
+    });
     overlays.default = final: prev: {
       deai = final.callPackage ./default.nix {};
     };
