ade646fabe6e97825ac793a0b64e6a8e4e390d07
Yuxuan Shui <yshuiv7@gmail.com>
2024-07-10 01:13:56 +0100

    flake: add a clang based dev shell
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,18 +1,23 @@
 {
   outputs = { self, nixpkgs, ...}:
-  let 
+  let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     deai = pkgs.callPackage ./default.nix {};
 
   in rec {
     packages.${system}.deai = deai;
-    devShell.${system} = deai.overrideAttrs (o: {
-      nativeBuildInputs = (with pkgs; [
-        clippy clang-tools_17
-      ]) ++ o.nativeBuildInputs;
-      hardeningDisable = [ "all" ];
-    });
+    devShells.${system} = rec {
+      default = deai.overrideAttrs (o: {
+        nativeBuildInputs = (with pkgs; [
+          clippy clang-tools_17
+        ]) ++ o.nativeBuildInputs;
+        hardeningDisable = [ "all" ];
+      });
+      clangEnv = default.override {
+        stdenv = pkgs.llvmPackages_latest.stdenv;
+      };
+    };
     overlays.default = final: prev: {
       deai = final.callPackage ./default.nix {};
     };
