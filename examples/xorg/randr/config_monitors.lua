4d381baf44c7171088d0b43895c4c8c8e0218be4
Yuxuan Shui <yshuiv7@gmail.com>
2024-07-15 11:02:12 +0100

    examples: add config_monitors
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

---
+++ examples/xorg/randr/config_monitors.lua
@@ -0,0 +1,19 @@
+xc = di.xorg:connect()
+
+-- Monitor models chosen randomly
+
+local output_configs = {
+	{
+		match = { model = "PHILIPS 241V8LA" },
+		-- Put Dell to the left of Philips
+		left = { { model = "DELL U2515H" } },
+	},
+	{
+		match = { model = "DELL U2515H" }
+	}
+}
+
+ret = xc.randr:configure_outputs(output_configs)
+if ret ~= nil and ret.errmsg ~= nil then
+	print(ret.errmsg)
+end
