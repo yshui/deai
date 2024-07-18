3157f927ce2171d99d54b9f5c19e018127bc020b
Yuxuan Shui <yshuiv7@gmail.com>
2024-07-18 08:35:36 +0100

    flake: use explicit nixpkgs url
    Instead of using the one from registry.
    
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,22 +1,40 @@
 {
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs;
+  };
   outputs = { self, nixpkgs, ...}:
   let
     system = "x86_64-linux";
+    profilePkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        (final: prev: {
+          stdenv = prev.withCFlags "-fno-omit-frame-pointer" prev.stdenv;
+        })
+        (final: prev: {
+          llvmPackages_18 = prev.llvmPackages_18 // {
+            stdenv = final.withCFlags "-fno-omit-frame-pointer" prev.llvmPackages_18.stdenv;
+          };
+        })
+      ];
+    };
+    mkDevShell = pkgs: args: (pkgs.callPackage ./default.nix (args pkgs)).overrideAttrs (o: {
+      nativeBuildInputs = (with pkgs; [
+        clippy clang-tools_18
+      ]) ++ o.nativeBuildInputs;
+      hardeningDisable = [ "all" ];
+    });
     pkgs = nixpkgs.legacyPackages.${system};
-    deai = pkgs.callPackage ./default.nix {};
-
   in rec {
-    packages.${system}.deai = deai;
+    packages.${system}.deai = pkgs.callPackage ./default.nix {};
     devShells.${system} = rec {
-      default = deai.overrideAttrs (o: {
-        nativeBuildInputs = (with pkgs; [
-          clippy clang-tools_18
-        ]) ++ o.nativeBuildInputs;
-        hardeningDisable = [ "all" ];
+      default = mkDevShell pkgs (_: {});
+      clangEnv = mkDevShell pkgs (pkgs: {
+        stdenv = pkgs.llvmPackages_18.stdenv;
       });
-      clangEnv = default.override {
-        stdenv = pkgs.llvmPackages_latest.stdenv;
-      };
+      clangProfileEnv = mkDevShell profilePkgs (pkgs: {
+        stdenv = pkgs.llvmPackages_18.stdenv;
+      });
     };
     overlays.default = final: prev: {
       deai = final.callPackage ./default.nix {};
